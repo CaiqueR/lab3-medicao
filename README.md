e856bcf9e787f14db216585c543afd1cfd29a717
Caique <caiiquexd@gmail.com>
2020-09-30 12:00:23 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Nome: Caique Ribeiro de Oliveira
+
+Para executar o projeto é necessário ter o NodeJS instalado.
+É necessário criar um arquivo na raiz do projeto com nome .env e colocar GITHUB_TOKEN=Seu token
+
+Após ter feito isso é só executar npm install e node src/resposta1.js e será gerado o csv na raíz do projeto.
+
+O código fonte pode ser encontrado em src/resposta(numero da questão).js e o CSV pode ser encontrado em csv/resposta(numero da questão).csv
---
+++ src/sprint1.js
@@ -0,0 +1,82 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-await-in-loop */
+const { Headers } = require("node-fetch");
+const converter = require("json-2-csv");
+const fs = require("fs");
+const axios = require("axios").default;
+
+require("dotenv").config();
+
+global.Headers = global.Headers || Headers;
+
+const repositoriosParaBuscar = ["java", "python"];
+
+async function resposta1() {
+  let repos = [];
+
+  repositoriosParaBuscar.forEach(async (repoParaBuscar) => {
+    const { data: result } = await axios.post(
+      "https://api.github.com/graphql",
+      {
+        query: `
+          query {
+            search(type: REPOSITORY query: "stars:>500 language:${repoParaBuscar}" first: 100) {
+                nodes {
+                  ... on Repository {
+                    nameWithOwner
+                    createdAt
+                    languages(orderBy: {field: SIZE, direction: DESC}, first: 1) {
+                      totalSize
+                      totalCount
+                      edges {
+                        size
+                        node {
+                          name
+                        }
+                      }
+                    }
+                    stargazers {
+                      totalCount
+                    }
+                    url
+                    sshUrl
+                  }
+                }
+                pageInfo {
+                  startCursor
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                }
+              }
+            
+          }
+      `,
+        variables: {},
+      },
+      {
+        headers: { Authorization: `bearer ${process.env.GITHUB_TOKEN}` },
+      }
+    );
+
+    const {
+      search: { nodes },
+    } = result.data;
+
+    repos = [...repos, ...nodes];
+
+    converter.json2csv(repos, (err, csv) => {
+      if (err) {
+        throw err;
+      }
+
+      fs.writeFileSync(`./csv/${repoParaBuscar}.csv`, csv);
+    });
+
+    repos = [];
+
+    console.log(`${repoParaBuscar} gerado na pasta csv com sucesso!`);
+  });
+}
+
+resposta1();
